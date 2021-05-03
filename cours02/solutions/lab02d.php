281326db2cd464d1e768bd7b31ad769f0367373d
Pier-Luc Brault <pier-luc@brault.me>
2021-05-03 15:24:01 -0400

    Solution lab 02 D

---
+++ cours02/solutions/lab02d.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Laboratoire 02 (D)
+    </title>
+    <link rel="stylesheet" type="text/css" href="lab01g.css" />
+  </head>
+  <body>
+    <?php
+      if (isset($_GET['ageEnfant'])) {
+        $ageEnfant = $_GET['ageEnfant'];
+
+        if ($ageEnfant > 13) {
+          echo 'Junior';
+        } else if ($ageEnfant > 11) {
+          echo 'Juvénie';
+        } else if ($ageEnfant > 9) {
+          echo 'Cadet';
+        } else if ($ageEnfant > 7) {
+          echo 'Benjamin';
+        } else {
+          echo 'Minime';
+        }
+      } else {
+        echo 'Erreur: Paramètre "<strong>ageEnfant</strong>" manquant.';
+      }
+    ?>
+  </body>
+</html>
