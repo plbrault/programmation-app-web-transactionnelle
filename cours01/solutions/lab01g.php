83827f5d7ffb7e32015971d7d26c6198c1fb5cb8
Pier-Luc Brault <plbrault@gmail.com>
2021-05-07 11:13:44 -0400

    Correction erreur dans solution lab01g

--- cours01/solutions/lab01g.php
+++ cours01/solutions/lab01g.php
@@ -8,9 +8,9 @@
   </head>
   <body>
     <?php
-      echo '<a href="lab01g.php?nombre=' . $_GET['nombre'] - 1 . '">&lt;</a>';
+      echo '<a href="lab01g.php?nombre=' . ($_GET['nombre'] - 1) . '">&lt;</a>';
       echo $_GET['nombre'];
-      echo '<a href="lab01g.php?nombre=' . $_GET['nombre'] + 1 . '">&gt;</a>';
+      echo '<a href="lab01g.php?nombre=' . ($_GET['nombre'] + 1) . '">&gt;</a>';
     ?>
   </body>
 </html>
