2e23de8562440ff02dc728c08df2b34b8a1432a8
Pier-Luc Brault <plbrault@gmail.com>
2021-05-10 09:35:53 -0400

    Corriger solution lab02f

--- cours02/solutions/lab02f.php
+++ cours02/solutions/lab02f.php
@@ -4,22 +4,42 @@
     <title>
       Laboratoire 02 (F)
     </title>
-    <link rel="stylesheet" type="text/css" href="lab02f.css" />
+    <link rel="stylesheet" type="text/css" href="lab01g.css" />
   </head>
   <body>
     <?php
-      if (isset($_GET['nombre'])) {
-        $nombre = $_GET['nombre'];
-
-        echo '<p class="';
-        if ($nombre %2 === 0) {
-          echo 'pair';
-        } else {
-          echo 'impair';
-        }
-        echo '">' . $nombre . '</p>';
+      if (!isset($_GET['nombre1'])) {
+        echo 'Paramètre manquant: "<strong>nombre1</strong>".';
+      } else if (!isset($_GET['nombre2'])) {
+        echo 'Paramètre manquant: "<strong>nombre2</strong>".';
+      } else if (!isset($_GET['operateur'])) {
+        echo 'Paramètre manquant: "<strong>operateur</strong>".';
       } else {
-        echo 'Paramètre manquant: "<strong>nombre</strong>".';
+        $nombre1 = $_GET['nombre1'];
+        $nombre2 = $_GET['nombre2'];
+        $operateur = $_GET['operateur'];
+
+        if ($operateur === '+') {
+          echo $nombre1 + $nombre2;
+        } else if ($operateur === '-') {
+          echo $nombre1 - $nombre2;
+        } else if ($operateur === '*') {
+          echo $nombre1 * $nombre2;
+        } else if ($operateur === '/') {
+          if ($nombre2 != 0) {
+            echo $nombre1 / $nombre2;
+          } else {
+            echo 'La division par 0 est interdite.';
+          }
+        } else if ($operateur === '%') {
+          if ($nombre2 != 0) {
+            echo $nombre1 % $nombre2;
+          } else {
+            echo 'Le modulo 0 est interdit.';
+          }
+        } else {
+          echo "L'opérateur fourni est invalide. Les opérateurs possibles sont: +, -, *, / et %.";
+        }
       }
     ?>
   </body>
