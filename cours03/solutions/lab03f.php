45b6eecb5366482325aa0721e32696c8596ac2c8
Pier-Luc Brault <pier-luc@brault.me>
2021-05-13 15:17:27 -0400

    Lab 03 e à g

---
+++ cours03/solutions/lab03f.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Laboratoire 03 (E)</title>
+  </head>
+  <body>
+    <select name="nombre">
+      <?php
+        for ($i = 1; $i <= 100; $i++) {
+          echo "<option value=\"$i\">$i</option>";
+        }
+      ?>
+    </select>
+  </body>
+</html>
