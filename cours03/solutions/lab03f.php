676c69a2245662b4af5b1099d776e7623de8efe7
Pier-Luc Brault <pier-luc@brault.me>
2021-05-13 15:34:10 -0400

    Corriger lab03e et lab03f

--- cours03/solutions/lab03f.php
+++ cours03/solutions/lab03f.php
@@ -5,6 +5,7 @@
   </head>
   <body>
     <select name="nombre">
+      <option value="">Sélectionnez un nombre</option>
       <?php
         for ($i = 1; $i <= 100; $i++) {
           echo "<option value=\"$i\">$i</option>";
