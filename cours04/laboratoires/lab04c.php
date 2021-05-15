5924ad467ee63e48a9256326afc7f2ef7622f23c
Pier-Luc Brault <pier-luc@brault.me>
2021-05-14 23:20:17 -0400

    Lab 04 C

---
+++ cours04/laboratoires/lab04c.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Laboratoire 04 (C)</title>
+  </head>
+  <body>
+    <?php
+      $nombres = array();
+      
+      for ($i = 0; $i <= 100; $i += 2) {
+        array_push($nombres, $i);
+      }
+    ?>
+
+    <select name="nombre">
+      <?php
+        foreach ($nombres as $nombre) {
+          echo "<option value=\"$nombre\">$nombre</option>";
+        }
+      ?>
+    </select>
+  </body>
+</html>
