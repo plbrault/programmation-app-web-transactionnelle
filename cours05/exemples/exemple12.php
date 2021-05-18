9e852b889e0e6df6b435c6863440596c7145a73f
Pier-Luc Brault <pier-luc@brault.me>
2021-05-18 13:53:02 -0400

    Exemple 12

---
+++ cours05/exemples/exemple12.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exemple 12</title>
+  </head>
+  <body>
+    <?php
+      if (isset($_POST['condiments']) && is_array($_POST['condiments'])) {
+        foreach ($_POST['condiments'] as $condiment) {
+          echo "$condiment <br />";
+        }
+      } else {
+        ?>
+
+        <form action="exemple12.php" method="POST">
+
+          <fieldset>
+            <legend>Que voulez-vous dans votre hot dog?</legend>
+            <p>
+              <!-- Envoi d'un tableau condiments -->
+              <input type="checkbox" id="ketchup-input" name="condiments[]" value="ketchup" />
+              <label for="ketchup-input">Ketchup</label>
+            </p>
+            <p>
+              <input type="checkbox" id="moutarde-input" name="condiments[]" value="moutarde" />
+              <label for="moutarde-input">Moutarde</label>     
+            </p>
+            <p>
+              <input type="checkbox" id="relish-input" name="condiments[]" value="relish" />
+              <label for="relish-input">Relish</label>     
+            </p>            
+          </fieldset>
+
+          <input type="submit" name="submit" />
+        </form>
+
+        <?php
+      }
+    ?>
+  </body>
+</html>
