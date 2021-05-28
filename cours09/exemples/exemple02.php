e764de58f0d7b13606cb7998ba3260e6f85686af
Pier-Luc Brault <pier-luc@brault.me>
2021-05-28 19:06:50 -0400

    Exemple 02

---
+++ cours09/exemples/exemple02.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Exemple 01
+    </title>
+  </head>
+  <body>
+    <?php
+      // Création de la classe Etudiant
+      class Etudiant {
+        // Propriétés
+        public $code;
+        public $nom;
+        public $prenom;
+
+        /*
+          Voici un constructeur.
+          Un constructeur est une méthode spéciale qui est appelée à la création d'une instance de la classe.
+          Il permet notamment d'initialiser des propriétés de l'objet.
+        */
+        function __construct($code, $nom, $prenom) {
+          $this->code = $code;
+          $this->nom = $nom;
+          $this->prenom = $prenom;
+        }
+
+        // Définition de la méthode afficher
+        function afficher() {
+          echo "[$this->code] $this->nom, $this->prenom";
+        }
+      }
+
+      // Instanciation de deux étudiants en passant des paramètres au constructeur
+      $etudiant1 = new Etudiant('200123456', 'Crête', 'Stéphane');
+      $etudiant2 = new Etudiant('207891011', 'Maynard', 'Mélanie');
+
+      // Affichage de l'étudiant 1
+      $etudiant1->afficher();
+      echo '<br />';
+
+      // Affichage de l'étudiant 2
+      $etudiant2->afficher();
+    ?>
+  </body>
+</html>
