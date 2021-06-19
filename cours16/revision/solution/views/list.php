b2f5e5197a1164d70820a16d8e05a27f3d4f9faf
Pier-Luc Brault <pier-luc@brault.me>
2021-06-18 22:49:50 -0400

    Add AJAX part

--- cours16/revision/solution/views/list.php
+++ cours16/revision/solution/views/list.php
@@ -6,30 +6,35 @@
   <body>
     <h1>Liste des rendez-vous</h1>
     <table>
-      <tr>
-        <th>Date</th>
-        <th>Heure</th>
-        <th>Nom</th>
-        <th>Prénom</th>
-        <th>Tél</th>
-        <th>Courriel</th>
-        <th># confirmation</th>
-      </tr>
-    <?php
-      foreach ($appointments as $appointment) {
-      ?>
+      <thead>
         <tr>
-          <td><?= $appointment['date'] ?></td>
-          <td><?= $appointment['time'] ?></td>
-          <td><?= $appointment['last_name'] ?></td>
-          <td><?= $appointment['first_name'] ?></td>
-          <td><?= $appointment['phone_number'] ?></td>
-          <td><?= $appointment['email'] ?></td>
-          <td><?= $appointment['confirmation_number'] ?></td>
+          <th>Date</th>
+          <th>Heure</th>
+          <th>Nom</th>
+          <th>Prénom</th>
+          <th>Tél</th>
+          <th>Courriel</th>
+          <th># confirmation</th>
         </tr>
-      <?php
-      }
-    ?>
+      </thead>
+      <tbody id="task_table_body">
+        <?php
+          foreach ($appointments as $appointment) {
+            ?>
+              <tr>
+                <td><?= $appointment['date'] ?></td>
+                <td><?= $appointment['time'] ?></td>
+                <td><?= $appointment['last_name'] ?></td>
+                <td><?= $appointment['first_name'] ?></td>
+                <td><?= $appointment['phone_number'] ?></td>
+                <td><?= $appointment['email'] ?></td>
+                <td><?= $appointment['confirmation_number'] ?></td>
+              </tr>
+            <?php
+          }
+        ?>
+      </tbody>
     </table>
+    <script src="public/js/list.js"></script>
   </body>
 </html>
