fa5b8fdee70461b09c01abe9b085ab546503ef40
Pier-Luc Brault <pier-luc@brault.me>
2021-06-25 18:02:08 -0400

    Exemple 2

---
+++ cours18/exemple02/views/connected.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bienvenue!</title>
+  </head>
+  <body>
+    <h1>Bienvenue!</h1>
+    <p>
+      Vous êtes connecté.e en tant que <strong><?= $username ?></strong>.
+    </p>
+  </body>
+</html>
---
+++ cours18/exemple02/views/login_success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Authentification réussie</title>
+  </head>
+  <body>
+    <h1>Authentification réussie</h1>
+    <p>
+      Félicitations, vous avez entré le bon mot de passe!
+    </p>
+  </body>
+</html>
