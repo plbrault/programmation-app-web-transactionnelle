bc0200fb8c31cc9c9e242e693b4c91ceadf664b7
Pier-Luc Brault <pier-luc@brault.me>
2021-07-06 11:22:48 -0400

    Ajout documentation

--- cours21/exemple/src/classes/Etudiant.php
+++ cours21/exemple/src/classes/Etudiant.php
@@ -2,22 +2,40 @@
 
 include_once('Cours.php');
 
+/**
+ * Représente un.e étudiant.e avec son code, son nom, son prénom et la liste de ses cours.
+ */
 class Etudiant {
   private $code;
   private $nom;
   private $prenom;
   private $cours = [];
 
+  /**
+   * Constructeur d'Etudiant.
+   * 
+   * @param string $code Le code de l'étudiant, ex: '202012345'.
+   * @param string $nom Le nom de l'étudiant.
+   * @param string $prenom Le prénom de l'étudiant.
+   */
   function __construct($code, $nom, $prenom) {
     $this->code = $code;
     $this->nom = $nom;
     $this->prenom = $prenom;
   }
   
+  /**
+   * Affiche l'étudiant à l'écran au format "[code] nom, prenom".
+   */
   function afficher() {
     echo "[$this->code] $this->nom, $this->prenom";
   }
 
+  /**
+   * Ajoute un cours à la liste de cours de l'étudiant.
+   * 
+   * @param Cours $cours Le cours à ajouter.
+   */
   function ajouterCours($cours) {
     if (!($cours instanceof Cours)) {
       throw new Exception('Le cours à ajouter doit être une instance de la classe Cours.');
@@ -26,6 +44,9 @@ class Etudiant {
     array_push($this->cours, $cours);
   }
 
+  /**
+   * Affiche à l'écran la liste des cours de l'étudiant dans une liste non ordonnée.
+   */
   function afficherCours() {
     echo '<ul>';
     foreach ($this->cours as $unCours) {
@@ -36,18 +57,38 @@ class Etudiant {
     echo '</ul>';
   }
 
+  /**
+   * Récupère le code de l'étudiant.
+   * 
+   * @return string
+   */
   function getCode() {
     return $this->code;
   }
 
+  /**
+   * Récupère le nom de l'étudiant.
+   * 
+   * @return string
+   */
   function getNom() {
     return $this->nom;
   }
 
+  /**
+   * Récupère le prénom de l'étudiant.
+   * 
+   * @return string
+   */
   function getPrenom() {
     return $this->prenom;
   }
 
+  /**
+   * Récupère la liste des cours de l'étudiant.
+   * 
+   * @return Cours[]
+   */
   function getCours() {
     return $this->cours;
   }
